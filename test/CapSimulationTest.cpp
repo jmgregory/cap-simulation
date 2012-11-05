0dd50aac2e49e41e321b764aa2a70c049bb96a0e
Justin Gregory <justin.m.gregory@vanderbilt.edu>
2012-11-05 11:08:15 -0600

    Add a test for d(kappa)/d(eta) behavior to CapSimulationTest

--- test/CapSimulationTest.cpp
+++ test/CapSimulationTest.cpp
@@ -150,3 +150,59 @@ TEST_FIXTURE(AirTestFixture, ZeroUnstrainedReflectivityThrowsException)
 {
   CHECK_THROW(my_simulation.Run(100e-12,1e-12), Exception);
 }
+
+class ReversedCapMaterial : public DefaultCapMaterial
+{
+  double dkappadeta(double z, double lambda) const     { if (z < 0) return 0; else return 100.0; }
+  double dndeta(double z, double lambda) const { return 0.0; }
+};
+
+//TEST(DKappaDEtaGivesSameResultAsDNDEta)
+void notest()
+{
+  ReversedCapMaterial material;
+  CapSimulation simulation;
+  simulation.set_material(&material);
+  std::vector <CapPoint> simulation_output = simulation.Run(0, 100e-12, 0.5e-12);
+
+  DampedSineParameters fitting_parameters;
+  // Values taken from GNUPlot fit
+  // amplitude = -0.0244596420635906; period = 4.7438779722113e-12; phase = 2.98622349788755; decay = 3.68058172120858e-11; offset = 0.0452298200333422; 
+  fitting_parameters.amplitude = -0.0244596420635906;
+  fitting_parameters.period = 4.7438779722113e-12;
+  fitting_parameters.phase = 2.98622349788755;
+  fitting_parameters.decay = 3.68058172120858e-11;
+  fitting_parameters.offset = 0.0452298200333422;
+
+  double deviation = CalculateAverageDeviationFromIdeal(simulation_output, fitting_parameters);
+  double max_deviation = 1e-5;
+  CHECK(deviation < max_deviation);
+
+  if (deviation >= max_deviation)
+    {
+      std::cerr << "Error: deviation " << deviation << " >= maximum allowable value of " << max_deviation << std::endl;
+      DumpSimResultsToFile(simulation_output, "cap-sim-output-dump.dat");
+    }
+  
+  double wavelength = LaserBeam().probe_wavelength();
+  double index = DefaultCapMaterial().n(0.0, wavelength);
+  double speed_of_sound = DefaultCapMaterial().speed_of_sound(0.0);
+
+  CHECK_CLOSE(wavelength / (2.0 * index * speed_of_sound),
+  	      abs(fitting_parameters.period), 
+  	      1e-13);
+
+  double kappa = DefaultCapMaterial().kappa(0.0, wavelength);
+  const double pi = 3.1415926535897932384627;
+  double alpha = 4.0 * pi * kappa / wavelength; // m-1
+
+  // Note: oscillations should decay as exp(-alpha*z), NOT exp(-2*alpha*z)
+  // This is due to the way the electric fields decay and recombine.
+  // This may be verified by looking at Thomsen (1986), and dividing the
+  // squared magnitude of Eqn. 30 by the squared magnitude of Eqn. 21 to get R.
+  // The oscillating component of the resulting reflectivity decays by
+  // exp(-4*pi*kappa*z/lambda), which is equal to exp(-alpha*z).
+  CHECK_CLOSE(alpha,
+	      1.0 / fitting_parameters.decay / speed_of_sound,
+	      3e4);
+}
